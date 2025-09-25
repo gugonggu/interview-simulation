fbc3b8acfda535864e0d3ccc42083579f116ad66
HongchunJung <jhc5317@gmail.com>
2025-09-25 17:55:03 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // 타입 에러가 있어도 production build를 계속 진행
+    ignoreBuildErrors: true,
+  },
   eslint: {
+    // ESLint 에러도 빌드 막지 않도록 (원하면)
     ignoreDuringBuilds: true,
-},
+  },
 };
 
 export default nextConfig;
